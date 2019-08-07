22b46c8ecd11a8669cd04f93eb883bcc4f806cdf
rami21-meet <rami21@meet.mit.edu>
2019-08-07 05:27:38 -0400

    Stopping here

--- lab_10_copy.py
+++ lab_10_copy.py
@@ -1,21 +1,31 @@
+### copy for experimentation with moving fish
+
 import turtle
 import random
 import time
 turtle.tracer(1,0) #This helps the turtle move more smoothly
-turtle.penup()
 
-fish_pos=[]
-fish_stamps=[]
 SIZE_X=800
 SIZE_Y=500
 SQUARE_SIZE=20
+START_LENGTH = 1
 
-fish=turtle.clone()
-fish.shape("circle")
-turtle.hideturtle()
-fish.hideturtle()
+UP_EDGE = 250
+DOWN_EDGE = -250
+RIGHT_EDGE = 400
+LEFT_EDGE = -400
 
-def apper_fish():
+fishtemplate = turtle.clone()
+fishtemplate.shape("square")
+
+fish1=fishtemplate.clone()
+fish1.hideturtle()
+fish1.direction='Up'
+fish1_stamps = []
+fish1_poses = []
+
+
+def random_goto(fish):
     min_x=-int(SIZE_X/2/SQUARE_SIZE)+1
     max_x=int(SIZE_X/2/SQUARE_SIZE)-1
     min_y=-int(SIZE_Y/2/SQUARE_SIZE)+1
@@ -25,34 +35,8 @@ def apper_fish():
     fish_y=random.randint(min_y,max_y)*SQUARE_SIZE
 
     fish.goto(fish_x,fish_y)
-    fish_pos.append(fish.pos())
-    f_stamps=fish.stamp()
-    fish_stamps.append(f_stamps)
 
-'''
-turtle.ontimer(apper_fish,1500)
-    
-for number in range(4):
-    
-    '''
-apper_fish()
-
-starttime=time.time()
-while True:
-  apper_fish()
-  time.sleep(5 - ((time.time() - starttime) % 5))
-  
-if robot.pos() in fish_pos:
-    print('game over! you lost!')
-    quit()
-
-
-
-
-
-"""
-#function that makes new stamp for the fish
-def new_stamp():
+def new_stamp(fish, stamp_list, pos_list):
     #i wanna get the current pos of the fish
     fish_pos = fish.pos()
     #and then make it appear in the pos list
@@ -62,69 +46,13 @@ def new_stamp():
     r=fish.stamp()
          
     stamp_list.append(r)
-"""
     
-#a for loop that draws the fish at the beginning of the game
-#it counts the number of pieces of the fish which is 1
-for b in range(START_LENGTH) :
-    x_pos=fish.pos()[0]
-    y_pos=fish.pos()[1] 
-    #add the size of the square (20) to x pos
-    x_pos+=SQUARE_SIZE
-
-    fish.goto(x_pos,y_pos)
-    #Move snake to new (x,y)
-   
-    #draw the next place of the fish
-    new_stamp()
-
-
-def remove_last_stamp(): #BTW i know its a long name, i couldn't find any better
-
+def remove_last_stamp(fish, stamp_list, pos_list): #BTW i know its a long name, i couldn't find any better
     old_stamp = stamp_list.pop(0) # get the ID of the last piece of body
     fish.clearstamp(old_stamp) # erase this last piece of body from the screen
     pos_list.pop(0) # remove last piece of the body position from the pos list
 
-
-#make the turtle direction to be up
-fish.direction='Up'
-
-#make variable of the the edges for later use. i don't wanna forget it
-UP_EDGE = 250
-DOWN_EDGE = -250
-RIGHT_EDGE = 400
-LEFT_EDGE = -400
-
-#define the keys
-def up():
-    fish.direction="Up"
-
-def down():
-    fish.direction="Down"
-
-
-def left():
-    fish.direction="Left"
-
-
-def right():
-    fish.direction="Right"
-
-#listen to the keys to change directions
-
-turtle.onkeypress(up, "Up")
-
-turtle.onkeypress(down, "Down")
-
-turtle.onkeypress(left, "Left")
-
-turtle.onkeypress(right, "Right")
-
-
-
-
-#and finally, the move function
-def move():
+def move(fish, stamp_list, pos_list):
     #get the pos and save it so that i will be able to change it
     my_pos = fish.pos()
     x_pos = my_pos[0]
@@ -145,14 +73,11 @@ def move():
         fish.goto(x_pos+SQUARE_SIZE, y_pos)
 
 
-    #makes the fish move automatically
-    turtle.ontimer(move,TIME_STEP)
-
     #remove the last stamp of the fish to make it "move"
     #TBH functions are not too bad
-    remove_last_stamp()
+    remove_last_stamp(fish, stamp_list, pos_list)
     #after i remove the last stamp i make the next stamp. thanks functions :P
-    new_stamp()
+    new_stamp(fish, stamp_list, pos_list)
 
    #where is my fish's new position???
     new_pos = fish.pos()
@@ -174,53 +99,48 @@ def move():
 
     if new_y_pos>=UP_EDGE:
         fish.direction="Down"
+
+
+random_goto(fish1)
+new_stamp(fish1, fish1_stamps, fish1_poses)
+move(fish1, fish1_stamps, fish1_poses)
+move(fish1, fish1_stamps, fish1_poses)
+move(fish1, fish1_stamps, fish1_poses)
+move(fish1, fish1_stamps, fish1_poses)
+
+'''
+
+
+turtle.ontimer(apper_fish,1500)
+    
+for number in range(4):
+    
+apper_fish()
+
+starttime=time.time()
+while True:
+  apper_fish()
+  time.sleep(5 - ((time.time() - starttime) % 5))
+  
+if robot.pos() in fish_pos:
+    print('game over! you lost!')
+    quit()
+
+"""
+#function that makes new stamp for the fish
+
+"""
+
+
+
+#and finally, the move function
 """
     if len(trash_stamps)<=6:
         make_trash()
 """
-    
+
 #now use the move!
-move()    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+'''
 
 
 turtle.mainloop()
